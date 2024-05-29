cbd92ad6918248f5ec1d15396909c3783d6039f2
Marco De Felice <marco.defelice@qwentes.it>
2024-05-29 07:42:09 +0200

    added logtail configuration on separated file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,7 @@
 <?php
 // Autoload Composer dependencies
-
 use \Illuminate\Support\Carbon as Date;
 use Illuminate\Support\Facades\Facade;
-use Monolog\Level;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -23,46 +21,8 @@ $capsule->addConnection($connections['mysql']);
 $capsule->bootEloquent();
 $capsule->setAsGlobal();
 
-//setup log level from env
-switch(env('APP_LOG_LEVEL','debug')) {
-    case 'debug':
-        $logLevel = Level::Debug;
-        break;
-    case 'info':
-        $logLevel = Level::Info;
-        break;
-    case 'notice':
-        $logLevel = Level::Notice;
-        break;
-    case 'warning':
-        $logLevel = Level::Warning;
-        break;
-    case 'error':
-        $logLevel = Level::Error;
-        break;
-    case 'critical':
-        $logLevel = Level::Critical;
-        break;
-    case 'alert':
-        $logLevel = Level::Alert;
-        break;
-    case 'emergency':
-        $logLevel = Level::Emergency;
-        break;
-    default:
-        $logLevel = Level::Debug;
-}
-
-//setup log with BetterStack
-$logger = new \Monolog\Logger(env('APP_NAME'));
-$logger→pushHandler(new \Logtail\Monolog\LogtailHandler(env('LOGTAIL_API_KEY')));
-
-// log on FS
-$logPath = env('APP_LOG_PATH',__DIR__.'/../storage/logs/log-'.date("Ymd").'.log');
-$streamHandler = new \Monolog\Handler\StreamHandler($logPath, $logLevel);
-$formatter = new \Monolog\Formatter\SyslogFormatter();
-$streamHandler->setFormatter($formatter);
-$logger->pushHandler($streamHandler);
+// Set up the logger
+require_once __DIR__ . '/../config/logger.php';
 
 // Set up the Facade application
 Facade::setFacadeApplication([
---
+++ config/logger.php
@@ -0,0 +1,45 @@
+<?php
+
+
+//setup log level from env
+switch(env('APP_LOG_LEVEL','debug')) {
+    case 'debug':
+        $logLevel = \Monolog\Level::Debug;
+        break;
+    case 'info':
+        $logLevel = \Monolog\Level::Info;
+        break;
+    case 'notice':
+        $logLevel = \Monolog\Level::Notice;
+        break;
+    case 'warning':
+        $logLevel = \Monolog\Level::Warning;
+        break;
+    case 'error':
+        $logLevel = \Monolog\Level::Error;
+        break;
+    case 'critical':
+        $logLevel = \Monolog\Level::Critical;
+        break;
+    case 'alert':
+        $logLevel = \Monolog\Level::Alert;
+        break;
+    case 'emergency':
+        $logLevel = \Monolog\Level::Emergency;
+        break;
+    default:
+        $logLevel = \Monolog\Level::Debug;
+}
+
+//setup log with BetterStack
+$logger = new \Monolog\Logger(env('APP_NAME'));
+if(env('APP_ENV') == 'prod') {
+    $logger->pushHandler(new \Logtail\Monolog\LogtailHandler(env('LOGTAIL_API_KEY')));
+}
+
+// log on FS
+$logPath = env('APP_LOG_PATH',__DIR__.'/../storage/logs/log-'.date("Ymd").'.log');
+$streamHandler = new \Monolog\Handler\StreamHandler($logPath, $logLevel);
+$formatter = new \Monolog\Formatter\SyslogFormatter();
+$streamHandler->setFormatter($formatter);
+$logger->pushHandler($streamHandler);
