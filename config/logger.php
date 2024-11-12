e6d6e575a2fd04886b670682ac4f6dc8df133718
BudgetControl <140796512+BudgetControl@users.noreply.github.com>
2024-11-12 10:02:44 +0100

    Update logger.php

--- config/logger.php
+++ config/logger.php
@@ -32,20 +32,18 @@ switch(env('APP_LOG_LEVEL','debug')) {
 }
 
 //setup log with BetterStack
-$logName = env('APP_NAME','BudgetControl');
-$logger = new \Monolog\Logger(env('LOG_NAME', $logName));
-
-// log rotation
-$logPath = env('APP_LOG_PATH', __DIR__.'/../storage/logs/log.log');
-$rotatingFileHandler = new \Monolog\Handler\RotatingFileHandler($logPath, (int) env('APP_LOG_ROTATION', 7), $logLevel);
-$rotatingFileHandler->setFormatter($formatter); 
+$logger = new \Monolog\Logger(env("APP_NAME","BudgetControl);
 
 // log on FS
-$formatter = new \Monolog\Formatter\LineFormatter('[%channel%][%level_name%] %message% %context% %extra%\n');
+$logPath = env('APP_LOG_PATH',__DIR__.'/../storage/logs/log-'.date("Ymd").'.log');
+$streamHandler = new \Monolog\Handler\StreamHandler($logPath, $logLevel);
+
+$formatter = new \Monolog\Formatter\LineFormatter("%datetime% > %level_name% > %message% %context% %extra%\n");
 $formatter->setJsonPrettyPrint(true);
 $formatter->includeStacktraces(true);
-$rotatingFileHandler->setFormatter($formatter);
-$logger->pushHandler($rotatingFileHandler);
+$streamHandler->setFormatter($formatter);
+$logger->pushHandler($streamHandler);
+
 
 // log on Logtail only in prod
 if(env('APP_ENV') == 'prod') {
